b24c3eb08a50dad901afa7519660b3f71cd7c6b4
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-04-26 16:46:58 +0530

    initialize project and connected database to server

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+
+import {connectDB} from './db/index.js';
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+app.get('/', (req, res)=> {
+    res.send("Hello World");
+})
+
+
+
+
+connectDB()
+.then(()=> {
+    app.listen(process.env.PORT|| 8000, ()=> {
+        console.log(`Server is running on port:${process.env.PORT}`);
+    })
+})
+.catch((err)=> {
+    console.log("MongoDB connection failed", err);
+})
+
